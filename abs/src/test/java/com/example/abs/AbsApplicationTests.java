0c914c6668521a78a1a2b232f46ce37d2f5cbc3a
=suriya <jayasuriya.s2024cse@sece.ac.in>
2025-10-19 10:01:31 +0530

    spring initialized

---
+++ abs/src/test/java/com/example/abs/AbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
